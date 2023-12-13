8b00e0c75b2656fe79c62e0a2601755522299e32
cleavor <86359265+cleavor@users.noreply.github.com>
2023-12-13 22:54:51 +0700

    Change everything to php

---
+++ sidebar.php
@@ -0,0 +1,92 @@
+<nav class="nav-sidebar" id="sidebar">
+    <div class="wrapper">
+      <div class="nav-toggle" id="nav-toggle">
+        <i class='bx bx-chevron-right'></i>
+      </div>
+      <div class="sidebar-inner">
+        <div class="logo-container">
+          <div class="logo-wrapper">
+            <div class="logo">
+              <img src="./assets/img/logo.png" alt="Logo">
+            </div>
+            <span class="text">Railway HR</span>
+          </div>
+        </div>
+        <div class="menu-container">
+          <div class="menu-header">
+            <div class="standalone-image">
+              <img src="./assets/img/profile.png" alt="Standalone Image">
+            </div>
+            <div class="standalone-image">
+              <span class=text>
+                A Person<br>
+                User ID: 821739
+              </span>
+            </div>
+            <a href="#" class="menu active">
+              <div class="menu-wrapper">
+                <div class="icon">
+                  <i class='bx bxs-grid-alt'></i>
+                </div>
+                <span class="text">
+                  Dashboard
+                </span>
+              </div>
+            </a>
+            <a href="#" class="menu">
+              <div class="menu-wrapper">
+                <div class="icon">
+                  <i class='bx bxs-id-card'></i>
+                </div>
+                <span class="text">
+                  Profile
+                </span>
+              </div>
+            </a>
+            <a href="#" class="menu">
+              <div class="menu-wrapper">
+                <div class="icon">
+                  <i class='bx bx-bar-chart-alt'></i>
+                </div>
+                <span class="text">
+                  Performance
+                </span>
+              </div>
+            </a>
+            <a href="#" class="menu">
+              <div class="menu-wrapper">
+                <div class="icon">
+                  <i class='bx bx-cabinet'></i>
+                </div>
+                <span class="text">
+                  Documentation
+                </span>
+              </div>
+            </a>
+            <a href="#" class="menu">
+              <div class="menu-wrapper">
+                <div class="icon">
+                  <i class='bx bxs-contact'></i>
+                </div>
+                <span class="text">
+                  Contact
+                </span>
+              </div>
+            </a>
+          </div>
+          <div class="menu-footer">
+            <a href="#" class="menu">
+              <div class="menu-wrapper">
+                <div class="icon">
+                  <i class='bx bx-power-off'></i>
+                </div>
+                <span class="text">
+                  Sign Out
+                </span>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
