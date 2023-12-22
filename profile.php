3ef093d8735b5f8436bbaacd08a58dc00d0b76eb
charamelatte <122473383+charamelatte@users.noreply.github.com>
2023-12-22 17:30:09 +0800

    Update profile.php

--- profile.php
+++ profile.php
@@ -18,51 +18,54 @@
     include("connect.php");
     ?>
 
-<div class="content-container">
-        <div class="profile-section">
-            <h2>Profile</h2>
-            <div class="profile-box">
-                <img src="./assets/img/profile-pic.jpg" alt="Profile Picture">
-                <div class="profile-edit-link">
-                    <a href="#">Edit Profile Picture</a>
+
+    <div class="content-container">
+        <div class="profile-background">
+            <div class="profile-section">
+                <h2>Profile</h2>
+                <div class="profile-box">
+                    <img src="./assets/img/profile-pic.jpg" alt="Profile Picture">
+                    <div class="profile-edit-link">
+                        <a href="#">Edit Profile Picture</a>
+                    </div>
+                </div>
+                <div class="profile-details">
+                    <div class="profile-field">
+                        
+                        <label for="firstName">First Name:</label>
+                        <input type="text" id="firstName" name="firstName">
+                            
+                        <label for="lastName">Last Name:</label>
+                        <input type="text" id="lastName" name="lastName">
+                            
+                        <label for="email">Email:</label>
+                        <input type="email" id="email" name="email">
+                    
+                    
+                        <label for="phoneNumber">Phone Number:</label>
+                        <input type="tel" id="phoneNumber" name="phoneNumber">
+                    
+                    
+                        <label for="address">Address:</label>
+                        <input type="text" id="address" name="address">
+                
+                    
+                        <label for="poscode">Poscode:</label>
+                        <input type="text" id="poscode" name="poscode">
+                    
+                    
+                        <label for="password">Password:</label>
+                        <input type="password" id="password" name="password">
+                    
+                        <div class="profile-buttons">
+                            <button class="cancel-button">Cancel</button>
+                            <button class="save-button">Save</button>
+                        </div>
+                    </div>    
                 </div>
             </div>
-            <div class="profile-details">
-                <div class="profile-field">
-                    
-                    <label for="firstName">First Name:</label>
-                    <input type="text" id="firstName" name="firstName">
-                        
-                    <label for="lastName">Last Name:</label>
-                    <input type="text" id="lastName" name="lastName">
-                        
-                    <label for="email">Email:</label>
-                    <input type="email" id="email" name="email">
-                
-                
-                    <label for="phoneNumber">Phone Number:</label>
-                    <input type="tel" id="phoneNumber" name="phoneNumber">
-                
-                
-                    <label for="address">Address:</label>
-                    <input type="text" id="address" name="address">
-            
-                
-                    <label for="poscode">Poscode:</label>
-                    <input type="text" id="poscode" name="poscode">
-                
-                
-                    <label for="password">Password:</label>
-                    <input type="password" id="password" name="password">
-                
-                    <div class="profile-buttons">
-                        <button class="cancel-button">Cancel</button>
-                        <button class="save-button">Save</button>
-                    </div>
-                </div>    
-            </div>
         </div>
-</div>
+    </div>
 
 <!-- Javascript -->
 <script src="./assets/js/script.js"></script>
