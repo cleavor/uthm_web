13d1fe7fc2b4d48bf7fe6d04f67356a81ba872ca
VictoriaCCC <121749663+VictoriaCCC@users.noreply.github.com>
2023-12-29 21:45:11 +0800

    Add files via upload

---
+++ performance.php
@@ -0,0 +1,32 @@
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Performance</title>
+   <!-- CSS -->
+   <link rel="stylesheet" href="css/style.css" />
+   <!-- Box Icons -->
+   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+
+<body>
+    <?php
+    include("sidebar.php");
+    ?>
+
+    <div class="content-container">
+        <h1 class="title2" style="margin-left:370px;padding-top:0px">Performance</h1>
+
+        <div class="button-box2">
+            <a href="attendance.php"><button class="right">Attendance</button></a>
+            <a href="workstatus.php"><button class="right">Work Status</button></a>
+            <a href="sickdays.php"><button class="right">Sick Days Tracking</button></a>
+            <a href="payroll.php"><button class="right">Payroll</button></a>
+        </div>
+    </div>
+
+<!-- Javascript -->
+<script src="js/script.js"></script>
+</body>
+
+</html>
