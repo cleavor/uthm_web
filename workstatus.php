63546b4b424f261ad1536fe56ac45f014caf368a
VictoriaCCC <121749663+VictoriaCCC@users.noreply.github.com>
2023-12-29 21:41:37 +0800

    Update workstatus.php

--- workstatus.php
+++ workstatus.php
@@ -1,10 +1,19 @@
+<?php
+   include("connect.php");
+
+   if(isset($_GET["user_id"])){
+      $user_id = $_GET["user_id"];
+      $delete = mysqli_query($conn,"DELETE FROM workstatus_records WHERE user_id = $user_id");
+   }
+?>
+
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Sidebar Menu</title>
+  <title>Work Status</title>
   <!-- CSS -->
-  <link rel="stylesheet" href="./assets/css/style.css" />
+  <link rel="stylesheet" href="css/style.css" />
   <!-- Box Icons -->
   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
@@ -12,87 +21,74 @@
 
 <?php include ("sidebar.php")?>
 
+  <!-- Content -->
+  <div class="content-container">
+    <p class="path_leave">PERFORMANCE > <a href="workstatus.php">WORK STATUS</a></p>
 
-  <div class ="content-container">
+    <div class="search_container">
+         <form method="POST" action="">
+            <div class="textbox-container">
+               <label for="user_id" class="leave_label1">User ID</label>
+               <input type="text" id="user_id" name="search_user_id" placeholder="Enter User ID">
+         
+               <button type="submit" class="btnsearch" name="searchButton">Search</button>
+            </div>
+         </form>
+    </div><br>
 
-    
+    <h1 class="title">Work Status Summary</h1>
 
-    <h4 class="title">Performance > Work Status</h4><br><br><br>
-   
-   
-<div class="textbox-container">
-<form method="POST" action="">
-   <div class="textbox-container">
-      <label for="username1">USER ID</label>
-      <input type="text" id="username1" name="search_user_id" placeholder="(Input User ID)">
- 
+    <div class="summary_table">
+      <?php
+         include("connect.php");
 
-      <button type="submit" name="searchButton">Search</button>
-   </div>
-</form>
-</div><br>
+         // Check if search parameters are provided
+         if (isset($_POST['search_user_id']) ) {
+            $search_user_id = $_POST['search_user_id'];
+            
 
+            // Build the SQL query based on provided search parameters
+            $sql = "SELECT * FROM workstatus_records WHERE 1";
 
+            if (!empty($search_user_id)) {
+               $sql .= " AND user_id = '$search_user_id'";
+            }
+         } else {
+            // Fetch all data if no specific search parameters are provided
+            $sql = "SELECT * FROM workstatus_records";
+         }
 
-<h3 class="title">Work Status Summary</h3><br><br>
-<div class="container">
-<div class="box">
-<?php
-// Database connection settings
-$servername = "localhost";
-$dbname = "railway";
-$username = "root";
-$password = "";
+         $result = $conn->query($sql);
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+         if ($result->num_rows > 0) {
+            // Output data of each row
+            echo "<table border='1'>";
+            echo "<thead>";
+            echo "<tr><th>User ID</th><th>Name</th><th>Email</th><th>Position</th><th>Work Status</th></tr>";
+            echo "</thead>";
+            echo "<tbody>";
+            while ($row = $result->fetch_assoc()) {
+               echo "<tr><td>" . $row["user_id"];
+               echo "</td><td>" . $row["name"];
+               echo "</td><td>" . $row["email"];
+               echo "</td><td>" . $row["position"];
+               echo "</td><td>". $row["work_status"];
+               echo "</td><td> 
+                     <div class='btn-group'> 
+                        <a href='workstatus.php?user_id=$row[user_id]' class='btn2' value='Delete'>Delete</a>
+                     </div>";
+               echo "</td></tr>";
+            }
+            echo "</tbody>";
+            echo "</table>";
+         } else {
+            echo "No results found"; 
+         }
 
-if ($conn->connect_error) {
-   die("Connection failed: " . $conn->connect_error);
-}
-
-// Check if search parameters are provided
-if (isset($_POST['search_user_id']) ) {
-   $search_user_id = $_POST['search_user_id'];
-   
-
-   // Build the SQL query based on provided search parameters
-   $sql = "SELECT * FROM workstatus_records WHERE 1";
-
-   if (!empty($search_user_id)) {
-      $sql .= " AND user_id = '$search_user_id'";
-   }
-
-   
-} else {
-   // Fetch all data if no specific search parameters are provided
-   $sql = "SELECT * FROM workstatus_records";
-}
-
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-   // Output data of each row
-   echo "<table border='1'>";
-   echo "<thead>";
-   echo "<tr><th>USER ID</th><th>NAME</th><th>EMAIL</th><th>POSITION</th><th>WORK STATUS</th></tr>";
-   echo "</thead>";
-   echo "<tbody>";
-   while ($row = $result->fetch_assoc()) {
-      echo "<tr><td>" . $row["user_id"] . "</td><td>" . $row["name"] . "</td><td>" . $row["email"] . "</td><td>" . $row["position"].  "</td><td>". $row["work_status"] . "</td></tr>";
-   }
-   echo "</tbody>";
-   echo "</table>";
-} else {
-   
-
-   echo "No results found";
-   
-}
-
-$conn->close();
-?>
+         $conn->close();
+      ?>
 
   <!-- Javascript -->
-  <script src="./assets/js/script.js"></script>
+  <script src="js/script.js"></script>
 </body>
 </html>
