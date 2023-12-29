380b466935626c729653c217b152f322cc798b91
VictoriaCCC <121749663+VictoriaCCC@users.noreply.github.com>
2023-12-29 21:43:34 +0800

    Add files via upload

---
+++ documentation.php
@@ -0,0 +1,30 @@
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Documentation</title>
+   <!-- CSS -->
+   <link rel="stylesheet" href="css/style.css" />
+   <!-- Box Icons -->
+   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+
+<body>
+    <?php
+    include("sidebar.php");
+    ?>
+
+    <div class="content-container">
+        <h1 class="title" style="margin-left:360px;padding-top:50px">Documentation</h1>
+
+        <div class="button-box">
+            <a href="leave_request.php"><button class="left">Leave Requests</button></a>
+            <a href="others.php"><button class="left">Other Documents</button></a>
+        </div>
+    </div>
+
+<!-- Javascript -->
+<script src="js/script.js"></script>
+</body>
+
+</html>
