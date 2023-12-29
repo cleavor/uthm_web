b77f27bdeb860197f34823e23aeff0f27cfb34f0
VictoriaCCC <121749663+VictoriaCCC@users.noreply.github.com>
2023-12-29 21:40:47 +0800

    Update leave_request.php

--- leave_request.php
+++ leave_request.php
@@ -1,12 +1,21 @@
+<?php
+   include("connect.php");
+
+   if(isset($_GET["user_id"])){
+      $user_id = $_GET["user_id"];
+      $delete = mysqli_query($conn,"DELETE FROM leave_requests WHERE user_id = $user_id");
+   }
+?>
+
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <title>Sidebar Menu</title>
+  <title>Leave Requests</title>
 
   <!-- CSS -->
-  <link rel="stylesheet" href="./assets/css/style.css" />
+  <link rel="stylesheet" href="css/style.css" />
   
   <!-- Box Icons -->
   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
@@ -17,43 +26,75 @@
 
   <!-- Content -->
   <div class="content-container">
-    <p class="path_leave">DOCUMENTATION > <a href="leave_request.php">LEAVE REQUEST</a></p>
+    <p class="path_leave">DOCUMENTATION > <a href="leave_request.php">LEAVE REQUESTS</a></p>
 
     <div class="search_container">
-        <label for="user_id" class="leave_label1">User ID</label><br>
-        <input type="text" name="user_id" placeholder="Enter User ID">
-        <img src="./assets/img/search.png" class="search_icon1">
+         <form method="POST" action="">
+            <div class="textbox-container">
+               <label for="user_id" class="leave_label1">User ID</label>
+               <input type="text" id="user_id" name="search_user_id" placeholder="Enter User ID">
+         
+               <button type="submit" class="btnsearch" name="searchButton">Search</button>
+            </div>
+         </form>
+    </div><br>
 
-        <label for="date" class="leave_label2">Date</label><br>
-        <input type="text" name="date" placeholder="XX/XX/XXXX">
-        <img src="./assets/img/search.png" class="search_icon2">
-    </div>
+    <h1 class="title">Leave Requests Submission</h1>
 
-    <h1 class="title">Leave Requests</h1>
+    <div class="summary_table">
+    <?php
+         include("connect.php");
 
-    <div class="leave_table">
-        <table>
-            <tr>
-                <th>User ID</th>
-                <th>Name</th>
-                <th>Position</th>
-                <th>Date of Leave</th>
-                <th>Reason</th>
-                <th>Status</th>
-            </tr>
-            <tr>
-                <td>123</td>
-                <td>Chai</td>
-                <td>Manager</td>
-                <td>21/12/2022</td>
-                <td>Sick</td>
-                <td class="leave_status">Approved</td>
-            </tr>
-        </table>
-    </div>
-  </div>
+         // Check if search parameters are provided
+         if (isset($_POST['search_user_id']) ) {
+            $search_user_id = $_POST['search_user_id'];
+            
+
+            // Build the SQL query based on provided search parameters
+            $sql = "SELECT * FROM leave_requests WHERE 1";
+
+            if (!empty($search_user_id)) {
+               $sql .= " AND user_id = '$search_user_id'";
+            }
+         } else {
+            // Fetch all data if no specific search parameters are provided
+            $sql = "SELECT * FROM leave_requests";
+         }
+
+         $result = $conn->query($sql);
+
+         if ($result->num_rows > 0) {
+            // Output data of each row
+            echo "<table border='1'>";
+            echo "<thead>";
+            echo "<tr><th>User ID</th><th>Name</th><th>Position</th><th>Date of Leave</th><th>Reason</th><th>Status</th></tr>";
+            echo "</thead>";
+            echo "<tbody>";
+            while ($row = $result->fetch_assoc()) {
+               echo "<tr><td>" . $row["user_id"];
+               echo "</td><td>" . $row["name"];
+               echo "</td><td>" . $row["position"];
+               echo "</td><td>" . $row["date_of_leave"];
+               echo "</td><td>". $row["leave_reason"];
+               echo "</td><td>" . $row["leave_request_status"];
+               echo "</td><td> 
+               <div class='btn-group'> 
+                  <a href='leave_request.php?user_id=$row[user_id]' class='btn2' value='Delete'>Delete</a>
+               </div>";
+         echo "</td></tr>";
+            }
+            echo "</tbody>";
+            echo "</table>";
+         } 
+         
+         else {
+            echo "No results found";
+         }
+
+         $conn->close();
+      ?>
   
   <!-- Javascript -->
-  <script src="./assets/js/script.js"></script>
+  <script src="js/script.js"></script>
 </body>
 </html>
