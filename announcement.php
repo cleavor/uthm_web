e7460ca555543303f0efabfd55d966b1760ff449
VictoriaCCC <121749663+VictoriaCCC@users.noreply.github.com>
2023-12-29 21:39:41 +0800

    Update announcement.php

--- announcement.php
+++ announcement.php
@@ -1,12 +1,19 @@
-<html lang="en">
+<?php
+   include("connect.php");
 
+   if(isset($_GET["announcement_id"])){
+      $announcement_id = $_GET["announcement_id"];
+      $delete = mysqli_query($conn,"DELETE FROM announcements WHERE announcement_id = $announcement_id");
+   }
+?>
+
+<html lang="en">
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Railway HR</title>
+   <title>Announcement</title>
    <!-- CSS -->
-   <link rel="stylesheet" href="./assets/css/style.css" />
-   <link rel="stylesheet" href="./assets/css/a-style.css" />
+   <link rel="stylesheet" href="css/style.css" />
    <!-- Box Icons -->
    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
@@ -14,48 +21,88 @@
 <body>
     <?php
     include("sidebar.php");
-    include("connect.php");
     ?>
 
     <div class="content-container">
-        <div class="announcement-section">
-            <h2>Announcement</h2>
-            <div class="announcement-box">
-                <p>Edit Announcements</p>
+
+    <div class="search_container">
+         <form method="POST" action="">
+            <div class="textbox-container">
+               <label for="announcement_id" class="leave_label1" style="margin-left:10px">Announcement ID</label>
+               <input type="text" id="announcement_id" name="search_announcement_id" placeholder="Enter Announcement ID">
+         
+               <button type="submit" class="btnsearch" name="searchButton">Search</button>
             </div>
-            <hr class="divider">
-            <div class="announcement-box">
-                <div class="new-announcement">
-                    <p>Announcement Information</p>
-                    <div class="subject">
-                        <label for="subject">Subject:</label>
-                        <input type="text" id="subject" name="subject">
-                    </div>
-                    <div class="announcement-text">
-                        <label for="announcement">Announcement:</label>
-                        <textarea id="announcement" name="announcement" rows="4"></textarea>
-                    </div>
-                    <div class="announcement-buttons">
-                        <button class="cancel-button">Cancel</button>
-                        <button class="save-button">Save</button>
-                    </div>
-                </div>
-            </div>
-            <hr class="divider">
-            <div class="announcement-box">
-                <p>Existing Announcements</p>
-                <div class="existing-announcements">
-                    <!-- Previous announcements container goes here -->
-                    <div class="previous-announcement">
-                        <p>Previous Announcement Text</p>
-                        <button class="delete-button">Delete</button>
-                    </div>
-                </div>
-            </div>
+         </form>
+    </div><br>
+
+      <div class="title-btn">
+        <h1 class="title" style="margin-left:10px">Announcement List</h1>
+        <a href="add_announcement.php" class="btnsearch2">+</a>
+
+      </div>
+        <div class="announcement-box" style="background-color: #fff;padding: 20px;border-radius: 15px;box-shadow: 0 8px 16px rgba(0, 0, 0, 0.1);">
+         <?php
+         include("connect.php");
+
+         // Check if search parameters are provided
+         if (isset($_POST['search_announcement_id']) ) {
+            $search_announcement_id = $_POST['search_announcement_id'];
+            
+
+            // Build the SQL query based on provided search parameters
+            $sql = "SELECT * FROM announcements WHERE 1";
+
+            if (!empty($search_announcement_id)) {
+               $sql .= " AND announcement_id = '$search_announcement_id'";
+            }
+         } 
+         
+         else {
+            // Fetch all data if no specific search parameters are provided
+            $sql = "SELECT * FROM announcements";
+         }
+
+         $result = $conn->query($sql);
+
+         if ($result->num_rows > 0) {
+            // Output data of each row
+            echo "<table border='1'>";
+            echo "<thead>";
+            echo "<tr><th>Announcement ID</th><th>Subject</th><th>Description</th><th>Released Date</th><th>Action</th></tr>";
+            echo "</thead>";
+            echo "<tbody>";
+            while ($row = $result->fetch_assoc()) {
+               echo "<tr><td>" . $row["announcement_id"];
+               echo "</td><td>" . $row["subject"];
+               echo "</td><td>" . $row["description"];
+               echo "</td><td>" . $row["date_of_release"];
+               echo "</td><td> 
+                     <div class='btn-group'> 
+                        <a href='edit_announcement.php?announcement_id=$row[announcement_id]
+                        & subject=$row[subject] & description=$row[description] 
+                        & date_of_release=$row[date_of_release]' class='btn1' value='Edit'>Edit</a>
+                        <a href='announcement.php?announcement_id=$row[announcement_id]' class='btn2' value='Delete'>Delete</a>
+                     </div>";
+               echo "</td></tr>";
+            }
+            echo "</tbody>";
+            echo "</table>";
+         } else {
+            
+
+            echo "No results found";
+            
+         }
+
+         $conn->close();
+      ?>
+  
         </div>
     </div>
+
 <!-- Javascript -->
-<script src="./assets/js/script.js"></script>
+<script src="js/script.js"></script>
 </body>
 
 </html>
