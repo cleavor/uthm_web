8a03478fb445afae80237daa1d083c822ffad1ba
VictoriaCCC <121749663+VictoriaCCC@users.noreply.github.com>
2023-12-29 21:40:02 +0800

    Update attendance.php

--- attendance.php
+++ attendance.php
@@ -1,11 +1,20 @@
+<?php
+   include("connect.php");
+
+   if(isset($_GET["user_id"])){
+      $user_id = $_GET["user_id"];
+      $delete = mysqli_query($conn,"DELETE FROM attendance_records WHERE user_id = $user_id");
+   }
+?>
+
 <html lang="en">
 
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Sidebar Menu</title>
+   <title>Attendance</title>
    <!-- CSS -->
-   <link rel="stylesheet" href="./assets/css/style.css" />
+   <link rel="stylesheet" href="css/style.css" />
    <!-- Box Icons -->
    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
@@ -14,74 +23,79 @@
 
    <?php
    include("sidebar.php");
-   include("connect.php");
    ?>
 
+   <!-- Content -->
    <div class="content-container">
+    <p class="path_leave">PERFORMANCE > <a href="attendance.php">ATTENDANCE</a></p>
 
-      <h4 class="title">Performance > Attendance</h4><br>
-
-      <div class="textbox-container">
-
+    <div class="search_container">
          <form method="POST" action="">
             <div class="textbox-container">
-               <label for="username1">USER ID</label>
-               <input type="text" id="username1" name="search_user_id" placeholder="(Input User ID)">
-
-               <label for="username1">DATE</label>
-               <input type="text" id="username2" name="search_date" placeholder="(Input Date)">
-
-               <button type="submit" name="searchButton" class="button">Search</button>
+               <label for="user_id" class="leave_label1">User ID</label>
+               <input type="text" id="user_id" name="search_user_id" placeholder="Enter User ID">
+         
+               <button type="submit" class="btnsearch" name="searchButton">Search</button>
             </div>
          </form>
-      </div><br>
+    </div><br>
 
-      <h3 class="title">Attendance Summary</h3><br><br>
-      <div class="container">
-         <div class="box">
+      <h1 class="title">Attendance Summary</h1>
+      <div class="summary_table">
+    <?php
+         include("connect.php");
 
-            <?php
-            // Check if search parameters are provided
-            if (isset($_POST['search_user_id']) || isset($_POST['search_date'])) {
-               $search_user_id = $_POST['search_user_id'];
-               $search_date = $_POST['search_date'];
+         // Check if search parameters are provided
+         if (isset($_POST['search_user_id']) ) {
+            $search_user_id = $_POST['search_user_id'];
+            
 
-               // Build the SQL query based on provided search parameters
-               $sql = "SELECT * FROM attendance_records WHERE 1";
+            // Build the SQL query based on provided search parameters
+            $sql = "SELECT * FROM attendance_records WHERE 1";
 
-               if (!empty($search_user_id)) {
-                  $sql .= " AND user_id = '$search_user_id'";
-               }
-
-               if (!empty($search_date)) {
-                  $sql .= " AND date = '$search_date'";
-               }
-            } else {
-               // Fetch all data if no specific search parameters are provided
-               $sql = "SELECT * FROM attendance_records";
+            if (!empty($search_user_id)) {
+               $sql .= " AND user_id = '$search_user_id'";
             }
+         } else {
+            // Fetch all data if no specific search parameters are provided
+            $sql = "SELECT * FROM attendance_records";
+         }
 
-            $result = $conn->query($sql);
+         $result = $conn->query($sql);
 
-            if ($result->num_rows > 0) {
-               // Output data of each row
-               echo "<table border='1'>";
-               echo "<thead>";
-               echo "<tr><th>USER ID</th><th>NAME</th><th>EMAIL</th><th>POSITION</th><th>ATTENDANCE</th><th>DATE</th></tr>";
-               echo "</thead>";
-               echo "<tbody>";
-               while ($row = $result->fetch_assoc()) {
-                  echo "<tr><td>" . $row["user_id"] . "</td><td>" . $row["name"] . "</td><td>" . $row["email"] . "</td><td>" . $row["position"] . "</td><td>" . $row["attendance"] . "</td><td>" . $row["date"] . "</td></tr>";
-               }
-               echo "</tbody>";
-               echo "</table>";
-            } else {
-               echo "No results found";
+         if ($result->num_rows > 0) {
+            // Output data of each row
+            echo "<table border='1'>";
+            echo "<thead>";
+            echo "<tr><th>User ID</th><th>Name</th><th>Position</th><th>Attendance</th><th>Date</th></tr>";
+            echo "</thead>";
+            echo "<tbody>";
+            while ($row = $result->fetch_assoc()) {
+               echo "<tr><td>" . $row["user_id"];
+               echo "</td><td>" . $row["name"];
+               echo "</td><td>" . $row["position"];
+               echo "</td><td>" . $row["attendance"];
+               echo "</td><td>". $row["date"];
+               echo "</td><td> 
+                     <div class='btn-group'> 
+                        <a href='attendance.php?user_id=$row[user_id]' class='btn2' value='Delete'>Delete</a>
+                     </div>";
+               echo "</td></tr>";
             }
-            $conn->close();
-            ?>
+            echo "</tbody>";
+            echo "</table>";
+         } else {
+            
+
+            echo "No results found";
+            
+         }
+
+         $conn->close();
+      ?>
+
             <!-- Javascript -->
-            <script src="./assets/js/script.js"></script>
+            <script src="js/script.js"></script>
 </body>
 
 </html>
