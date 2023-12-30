f76d1278ec3630a538dc0e30b8427e72f32c4752
Eswarie24 <153592033+Eswarie24@users.noreply.github.com>
2023-12-30 23:48:10 +0800

    Update attendance.php

--- attendance.php
+++ attendance.php
@@ -14,7 +14,8 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Attendance</title>
    <!-- CSS -->
-   <link rel="stylesheet" href="css/style.css" />
+  <link rel="stylesheet" href="./assets/css/style.css" />
+  <link rel="stylesheet" href="./assets/css/a-style.css"/>
    <!-- Box Icons -->
    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
