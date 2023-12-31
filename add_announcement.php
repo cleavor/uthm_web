7b965ed86545b80182194ad39935159d563e455b
VictoriaCCC <121749663+VictoriaCCC@users.noreply.github.com>
2023-12-31 16:51:43 +0800

    Update add_announcement.php

--- add_announcement.php
+++ add_announcement.php
@@ -5,8 +5,8 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Add New Announcement</title>
    <!-- CSS -->
-   <link rel="stylesheet" href="./asset/css/style.css" />
-   <link rel="stylesheet" href="./asset/css/a-style.css" />
+   <link rel="stylesheet" href="./assets/css/style.css" />
+   <link rel="stylesheet" href="./assets/css/a-style.css" />
    <!-- Box Icons -->
    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
